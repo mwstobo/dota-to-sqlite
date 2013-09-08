cbdddeb3f1aeb30b071b282919713e9de522ea50
Matt Stobo <mwstobo@gmail.com>
2013-09-08 12:10:59 -0400

    Added a database class

---
+++ dotasql/database.py
@@ -0,0 +1,159 @@
+import os
+import sqlite3
+import tools
+
+class Database(object):
+	def __init__(self):
+		account_id = tools.get_cfg(tools.cfg_filename, "account-id")
+		db_file = "matches_{0}".format(account_id)
+		db_exists = os.path.exists(db_file)
+		self.db = sqlite3.connect(db_file)
+		if not db_exists:
+			self._setup_tables()
+		
+	def insert(self, table, values):
+		if table == "matches":
+			sql = """
+				INSERT INTO matches VALUES (
+					?, ?, ?, ?, ?, ?, ?, ?, ?,
+					?, ?, ?, ?,	?, ?, ?, ?
+				)
+			"""
+		elif table == "players":
+			sql = """
+				INSERT INTO players VALUES (
+					?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+					?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+				)
+			"""
+		elif table == "abilities":
+			sql = """
+				INSERT INTO abilities VALUES (
+					?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+					?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+					?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,	?
+				)
+			"""
+		else:
+			raise ValueError("{0} is not an allowed table!".format(table))
+		c = self.db.cursor()
+		c.execute(sql, values)
+		c.close()
+		self.db.commit()
+
+	def _setup_tables(self):
+		matches = """
+			CREATE TABLE matches (
+				id INTEGER,
+				radiant_win INTEGER,
+				duration INTEGER,
+				start_time INTEGER,
+				seq_num INTEGER,
+				radiant_tower INTEGER,
+				dire_tower INTEGER,
+				radiant_barracks INTEGER,
+				dire_barracks INTEGER,
+				cluster INTEGER,
+				first_blood INTEGER,
+				lobby_type INTEGER,
+				human_players INTEGER,
+				leagueid INTEGER,
+				positive_votes INTEGER,
+				negative_votes INTEGER,
+				game_mode INTEGER,
+				PRIMARY KEY(id)
+			)
+		"""
+		players = """
+			CREATE TABLE players (
+				match_id INTEGER,
+				account_id INTEGER,
+				player_slot INTEGER,
+				hero_id INTEGER,
+				item_0 INTEGER,
+				item_1 INTEGER,
+				item_2 INTEGER,
+				item_3 INTEGER,
+				item_4 INTEGER,
+				item_5 INTEGER,
+				kills INTEGER,
+				deaths INTEGER,
+				assists INTEGER,
+				leaver_status INTEGER,
+				gold INTEGER,
+				last_hits INTEGER,
+				denies INTEGER,
+				gpm INTEGER,
+				xpm INTEGER,	
+				gold_spent INTEGER,
+				hero_damage INTEGER,
+				tower_damage INTEGER,
+				hero_healing INTEGER,
+				level INTEGER,
+				PRIMARY KEY(match_id, player_slot)
+			)
+		"""
+		abilities = """
+			CREATE TABLE abilities (
+				match_id INTEGER,
+				account_id INTEGER,
+				player_slot INTEGER,
+				ability_0 INTEGER,
+				ability_0_time INTEGER,
+				ability_1 INTEGER,
+				ability_1_time INTEGER,
+				ability_2 INTEGER,
+				ability_2_time INTEGER,
+				ability_3 INTEGER,
+				ability_3_time INTEGER,
+				ability_4 INTEGER,
+				ability_4_time INTEGER,
+				ability_5 INTEGER,
+				ability_5_time INTEGER,
+				ability_6 INTEGER,
+				ability_6_time INTEGER,
+				ability_7 INTEGER,
+				ability_7_time INTEGER,
+				ability_8 INTEGER,
+				ability_8_time INTEGER,
+				ability_9 INTEGER,
+				ability_9_time INTEGER,
+				ability_10 INTEGER,
+				ability_10_time INTEGER,
+				ability_11 INTEGER,
+				ability_11_time INTEGER,
+				ability_12 INTEGER,
+				ability_12_time INTEGER,
+				ability_13 INTEGER,
+				ability_13_time INTEGER,
+				ability_14 INTEGER,
+				ability_14_time INTEGER,
+				ability_15 INTEGER,
+				ability_15_time INTEGER,
+				ability_16 INTEGER,
+				ability_16_time INTEGER,
+				ability_17 INTEGER,
+				ability_17_time INTEGER,
+				ability_18 INTEGER,
+				ability_18_time INTEGER,
+				ability_19 INTEGER,
+				ability_19_time INTEGER,
+				ability_20 INTEGER,
+				ability_20_time INTEGER,
+				ability_21 INTEGER,
+				ability_21_time INTEGER,
+				ability_22 INTEGER,
+				ability_22_time INTEGER,
+				ability_23 INTEGER,
+				ability_23_time INTEGER,
+				ability_24 INTEGER,
+				ability_24_time INTEGER,
+				PRIMARY KEY(match_id, player_slot)
+			)
+		"""
+		c = self.db.cursor()
+		c.execute(matches)
+		c.execute(players)
+		c.execute(abilities)
+		c.close()
+		self.db.commit()
--- dotasql/matches.py
+++ dotasql/matches.py
@@ -1,84 +1,94 @@
 import sqlite3
 import logging
 import tools
-import players
+from database import *
 
-account_id = tools.get_cfg(tools.cfg_filename, "account-id")
-conn = sqlite3.connect("matches_{0}".format(account_id))
-c = conn.cursor()
+db = Database()
 
-def _create_tables():
-	c.execute("""
-		CREATE TABLE matches (
-			id INTEGER,
-			radiant_win INTEGER,
-			duration INTEGER,
-			start_time INTEGER,
-			seq_num INTEGER,
-			radiant_tower INTEGER,
-			dire_tower INTEGER,
-			radiant_barracks INTEGER,
-			dire_barracks INTEGER,
-			cluster INTEGER,
-			first_blood INTEGER,
-			lobby_type INTEGER,
-			human_player_ids INTEGER,
-			leagueid INTEGER,
-			positive_votes INTEGER,
-			negative_votes INTEGER,
-			game_mode INTEGER,
-			PRIMARY KEY(id)
-		)
-	""")
-	conn.commit()
-
-def _check_tables():
-	c.execute("""
-		SELECT name FROM sqlite_master
-		WHERE type='table' AND name LIKE 'matches'
-	""")
-	if c.fetchone() == None:
-		_create_tables()
-		
-def add_match(info):
-	_check_tables()
+def add_abilities(info, match_id):
 	if type(info) is not dict:
 		raise TypeErorr("Excepted paramater to be dict!")
-	id = info['match_id']
-	radiant_win = int(info['radiant_win'])
-	duration = info['duration']
-	start_time = info['start_time']
-	seq_num = info['match_seq_num']
-	radiant_tower = info['tower_status_radiant']
-	dire_tower = info ['tower_status_dire']
-	radiant_barracks = info['barracks_status_radiant']
-	dire_barracks = info['barracks_status_dire']
-	cluster = info['cluster']
-	first_blood = info['first_blood_time']
-	lobby_type = info['lobby_type']
-	human_player_ids = info['human_players']
-	leagueid = info['leagueid']
-	positive_votes = info['positive_votes']
-	negative_votes = info['negative_votes']
-	game_mode = info['game_mode']
+	values = []
+	values.append(match_id)
+	values.append(info['account_id'])
+	values.append(info['player_slot'])
+	ability_ids = []
+	ability_times = []
+	if 'ability_upgrades' in info:
+		for ability in info['ability_upgrades']:
+			ability_ids.append(ability['ability'])
+			values.append(ability['ability'])
+			ability_times.append(ability['time'])
+			values.append(ability['time'])
+	while len(ability_ids) < 25:
+		ability_ids.append(0)
+		values.append(0)
+	while len(ability_times) < 25:
+		ability_times.append(0)
+		values.append(-1)
+	db.insert("abilities", tuple(values))
+
+def add_player(info, match_id):
+	if type(info) is not dict:
+		raise TypeErorr("Excepted paramater to be dict!")
+	values = []
+	values.append(match_id)
+	values.append(info['account_id'])
+	values.append(info['player_slot'])
+	values.append(info['hero_id'])
+	values.append(info['item_0'])
+	values.append(info['item_1'])
+	values.append(info['item_2'])
+	values.append(info['item_3'])
+	values.append(info['item_4'])
+	values.append(info['item_5'])
+	values.append(info['kills'])
+	values.append(info['deaths'])
+	values.append(info['assists'])
+	values.append(info['leaver_status'])
+	values.append(info['gold'])
+	values.append(info['last_hits'])
+	values.append(info['denies'])
+	values.append(info['gold_per_min'])
+	values.append(info['xp_per_min'])
+	values.append(info['gold_spent'])
+	values.append(info['hero_damage'])
+	values.append(info['tower_damage'])
+	values.append(info['hero_healing'])
+	values.append(info['level'])
+	add_abilities(info, match_id)
+	db.insert("players", tuple(values))
+		
+def add_match(info):
+	if type(info) is not dict:
+		raise TypeErorr("Excepted paramater to be dict!")
+	values = []
+	values.append(info['match_id'])
+	values.append(int(info['radiant_win']))
+	values.append(info['duration'])
+	values.append(info['start_time'])
+	values.append(info['match_seq_num'])
+	values.append(info['tower_status_radiant'])
+	values.append(info['tower_status_dire'])
+	values.append(info['barracks_status_radiant'])
+	values.append(info['barracks_status_dire'])
+	values.append(info['cluster'])
+	values.append(info['first_blood_time'])
+	values.append(info['lobby_type'])
+	values.append(info['human_players'])
+	values.append(info['leagueid'])
+	values.append(info['positive_votes'])
+	values.append(info['negative_votes'])
+	values.append(info['game_mode'])
 	for player in info['players']:
-		players.add_player(player, id)
-	data = (id, radiant_win, duration, start_time, seq_num, radiant_tower,
-			dire_tower, radiant_barracks, dire_barracks, cluster, first_blood,
-			lobby_type, human_player_ids, leagueid,	positive_votes,
-			negative_votes, game_mode)
-	c.execute("""
-		INSERT INTO matches VALUES (
-			?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-			?, ?, ?, ?
-		)
-	""", data)
-	conn.commit()
+		add_player(player, info['match_id'])
+	db.insert("matches", tuple(values))
 	
 def indexed(id):
-	_check_tables()
+	c = db.db.cursor()
 	c.execute("SELECT * FROM matches WHERE id=?", (id,))
 	if c.fetchone() == None:
 		return False
 	else:
 		return True
+	c.close()
